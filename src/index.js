70d088ae47a2eac13f19ed07664f602672d3fa24
b108108 <b108@tut.by>
2019-02-27 00:29:59 +0300

    Task 6 - money exchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    const money = {"H":50,"Q":25,"D":10,"N":5,"P":1}
+  
+    if (currency === 0) {
+        return {}
+    }
+    
+    if (currency >= 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+
+    for (let item in money) {
+        
+        if (currency >= money[item]) {
+            let numsqr = Math.trunc(currency / money[item])
+            currency -= money[item]*numsqr  
+            money[item] = numsqr           
+        } else {
+            delete money[item]
+        }        
+    }
+
+    return money
 }
